8af45317b8a9355c31cb1b296dd1e22a3a7e808e
Jasmine Ryon <jryon12@priorypride.net>
2022-01-17 11:14:43 -0800

    create Navlinks in Navbar.js

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,86 @@
 import React from 'react'
+import './Navbar.css';
+import {NavLink} from 'react-router-dom'
+
+const style = {
+    width: "60%",
+    margin: "5% 0 1%",
+    padding: "1em",
+    textDecoration: "none"
+}
 
 export const Navbar = () => {
     return (
         <div>
-            
+            <NavLink
+            activeStyle={{
+                fontWeight: "bolder",
+                color: "green"
+            }}
+                exact
+                style={style}
+                to="/"
+            >Home</NavLink>
+
+            <NavLink
+            activeStyle={{
+                fontWeight: "bolder",
+                color: "green"
+            }}
+                exact
+                style={style}
+                to="/about"
+            >About</NavLink>
+
+            <NavLink
+            activeStyle={{
+                fontWeight: "bolder",
+                color: "green"
+            }}
+                exact
+                style={style}
+                to="/gallery"
+            >Gallery</NavLink>
+
+            <NavLink
+            activeStyle={{
+                fontWeight: "bolder",
+                color: "green"
+            }}
+                exact
+                style={style}
+                to="/services"
+            >Services</NavLink>
+
+            <NavLink
+            activeStyle={{
+                fontWeight: "bolder",
+                color: "green"
+            }}
+                exact
+                style={style}
+                to="/commissions"
+            >Commissions</NavLink>
+
+            <NavLink
+            activeStyle={{
+                fontWeight: "bolder",
+                color: "green"
+            }}
+                exact
+                style={style}
+                to="/contact"
+            >Contact</NavLink>
+
+            <NavLink
+            activeStyle={{
+                fontWeight: "bolder",
+                color: "green"
+            }}
+                exact
+                style={style}
+                to="/shop"
+            >Shop</NavLink>
         </div>
     )
 }
