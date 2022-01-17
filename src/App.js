5f296fbf786b00f1e464641601fa0fcfadcfd45d
Jasmine Ryon <jryon12@priorypride.net>
2022-01-16 21:23:45 -0800

    set up app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      </header> */}
+      <Router>
+        <Navbar />
+        <Header />
+          <Route>
+            
+          </Route>
+        <Switch>
+        </Switch>
+      </Router>
     </div>
   );
 }
