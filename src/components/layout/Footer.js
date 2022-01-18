798e5135be02f43089efc2ef0924560b0bba32b7
Jasmine Ryon <jryon12@priorypride.net>
2022-01-17 16:54:28 -0800

    add social media icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,7 +4,14 @@ import {SocialMediaIconsReact} from 'social-media-icons-react';
 export const Footer = () => {
     return (
         <div>
-            
+            <div className="row" style={{display: "flex", height: "50px", marginBottom: "-50px"}}>
+                <div className="col-6" style={{width: "50%", float: "left"}}>
+                    <h5>Jasmine Ryon Productions</h5>
+                </div>
+                <div className="col-6" style={{width: "50%", float: "right"}}>
+                    <SocialMediaIconsReact borderColor="rgba(172,163,79,0.25)" borderWidth="1" borderStyle="solid" icon="instagram" iconColor="rgba(237,219,161,1)" backgroundColor="rgba(87,130,71,1)" iconSize="5" roundness="50%" url="https://www.instagram.com/xxgeminianxx/" size="30" /> <SocialMediaIconsReact borderColor="rgba(172,163,79,0.25)" borderWidth="1" borderStyle="solid" icon="twitter" iconColor="rgba(237,219,161,1)" backgroundColor="rgba(87,130,71,1)" iconSize="5" roundness="50%" url="https://twitter.com/Myalove09" size="30" /> <SocialMediaIconsReact borderColor="rgba(172,163,79,0.25)" borderWidth="1" borderStyle="solid" icon="facebook" iconColor="rgba(237,219,161,1)" backgroundColor="rgba(87,130,71,1)" iconSize="5" roundness="50%" url="https://www.facebook.com/mariah.vasquez.90475" size="30" />    <SocialMediaIconsReact borderColor="rgba(172,163,79,0.25)" borderWidth="1" borderStyle="solid" icon="pinterest" iconColor="rgba(237,219,161,1)" backgroundColor="rgba(87,130,71,1)" iconSize="5" roundness="50%" url="https://www.pinterest.com/myalove09/_saved/" size="30" />
+                </div>
+            </div>
         </div>
     )
 }
