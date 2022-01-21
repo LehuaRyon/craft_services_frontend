bce7792587f485077d73a204e3155ae65ac666e6
Jasmine Ryon <jryon12@priorypride.net>
2022-01-21 15:16:21 -0800

    render services cards

--- src/components/services/ServicesList.js
+++ src/components/services/ServicesList.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {ServiceCard} from './ServiceCard'
 
-export const ServicesList = () => {
+export const ServicesList = ({services}) => {
+    const servicesCards = services.map(service => <ServiceCard service={service} />)
+    // have a list of services objects
+    // services is an array, map it, for each service object, render service card component
+
     return (
         <div>
-            
+            {servicesCards}
         </div>
     )
 }
