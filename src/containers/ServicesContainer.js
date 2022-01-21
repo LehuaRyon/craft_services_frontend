1d61fca4796c54a90bb3ef1683dc7eca26cf7853
Jasmine Ryon <jryon12@priorypride.net>
2022-01-21 14:38:02 -0800

    create ServicesFilter

--- src/containers/ServicesContainer.js
+++ src/containers/ServicesContainer.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+import {ServicesList} from '../components/services/ServicesList'
+import {ServicesFilter} from '../components/services/ServicesFilter'
+
+// contains list of services, ServicesList
+// contains search or filter bar and needs list of services, ServicesFilter
+// every time fetch, gonna use state to store data
 
 export const ServicesContainer = () => {
+
+    
     return (
         <div>
-            
+            <ServicesFilter />
+            <ServicesList />
         </div>
     )
 }
