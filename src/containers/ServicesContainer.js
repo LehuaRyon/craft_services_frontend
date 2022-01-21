8667c6bf6226c84a9e5f27568e8cf7ac5fc8d7f0
Jasmine Ryon <jryon12@priorypride.net>
2022-01-21 14:59:46 -0800

    fetch services using await

--- src/containers/ServicesContainer.js
+++ src/containers/ServicesContainer.js
@@ -8,8 +8,32 @@ import {ServicesFilter} from '../components/services/ServicesFilter'
 // every time fetch, gonna use state to store data
 
 export const ServicesContainer = () => {
+    const[services, setServices] = useState([])
 
-    
+    // const fetchData = () => {
+    //     fetch("http://localhost:3001/services")
+    //     // fires http request, get promise
+    //     .then(response => response.json())
+    //     // when promise resolves, hten have response object
+    //     // returns a promise
+    //     .then(data => setServices(data))
+    //     // when promise resolves, then get data wanted
+    // }
+
+    const fetchData = async () => {
+        const resp = await fetch("http://localhost:3001/services")
+        //  try to fetch something & wait until having something befoe storing inside the resp variable
+        const data = await resp.json()
+        setServices(data)
+    }
+
+    useEffect(() => {
+        // hook, function, takes 2 arguments
+        // automatically sync
+        // like componentDidMount
+        fetchData()
+    }, [])
+
     return (
         <div>
             <ServicesFilter />
