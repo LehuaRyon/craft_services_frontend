99ce178317086360227ded9e668790d8bab659b8
Jasmine Ryon <jryon12@priorypride.net>
2022-01-21 17:48:37 -0800

    set up ServicesFilter component

--- src/components/services/ServicesFilter.js
+++ src/components/services/ServicesFilter.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import {useState} from 'react'
 
 export const ServicesFilter = () => {
+    const [search, setSearch] = useState("")
+
+    const handleChange = (e) => {
+        setSearch(e.target.value)
+    }
+    
+
     return (
         <div>
-            
+            <input type="text" placeholder="Search for a service..." value={search} onChange={handleChange}/>
         </div>
     )
 }
