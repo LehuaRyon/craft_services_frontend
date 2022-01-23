2b9d6ee6571693e6639759f76505884b253d8da9
Jasmine Ryon <jryon12@priorypride.net>
2022-01-23 14:00:27 -0800

    useEffect working with change in route params

--- src/components/services/ServiceShow.js
+++ src/components/services/ServiceShow.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import {useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 export const ServiceShow = () => {
-   const[chosenService, setChosenServices] = useState([])
+   const[chosenService, setChosenService] = useState([])
    const routeId = useParams().id
 
     const fetchService = async (id) => {
         try {
-            const resp = await fetch(`http://localhost:3001/services/${routeId}`)
+            const resp = await fetch(`http://localhost:3001/services/${id}`)
             const data = await resp.json()
-            setChosenServices(data)
+            setChosenService(data)
         } catch (error) {
             alert(error)
         }
@@ -18,15 +19,22 @@ export const ServiceShow = () => {
     }
 
     useEffect(() => {
-        // hook, function, takes 2 arguments
-        // automatically sync
-        // like componentDidMount
-        fetchService()
-    }, [])
+        fetchService(routeId)
+    }, [routeId])
+    
 
     return (
         <div>
             <h2>on service show page</h2>
+            <NavLink
+            activeStyle={{
+                fontWeight: "bolder",
+                color: "darkgreen"
+            }}
+                exact
+                // style={style}
+                to={`/services/${parseInt(routeId) + 1}`}
+            >See Next Service</NavLink>
         </div>
     )
 }
