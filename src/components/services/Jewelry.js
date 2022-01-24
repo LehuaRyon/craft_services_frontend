313bd6c792f981f22603f87e45758cebf938de33
Jasmine Ryon <jryon12@priorypride.net>
2022-01-24 13:05:49 -0800

    Deleted some unecessary files

--- src/components/services/Jewelry.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Jewelry = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
--- src/components/services/Stickers.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Stickers = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
